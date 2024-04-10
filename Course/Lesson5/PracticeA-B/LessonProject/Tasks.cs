a53be4d2f8edb2631e5127eabf35d489560c1313
Fellkings <fellkings51@gmail.com>
2024-04-10 10:29:45 +0300

    feat: complete tasks Lesson5

--- Course/Lesson5/PracticeA-B/LessonProject/Tasks.cs
+++ Course/Lesson5/PracticeA-B/LessonProject/Tasks.cs
@@ -10,7 +10,20 @@ namespace LessonProject
             Console.WriteLine("РЕШЕНИЕ:");
 
             //решайте задачу здесь
+            int[] newList = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 10};
+            int N = 3;
+            Array.Sort(newList);
+            int[] output = new int[N];
 
+            for(int i = 0; i < N; i++)
+            {
+                output[i] = newList[newList.Length-1-i];
+            }
+
+            foreach(int number in output)
+            {
+                Console.Write(number + " ");
+            }
             //не выходя за пределы
         }
     }
@@ -23,7 +36,25 @@ namespace LessonProject
             Console.WriteLine("РЕШЕНИЕ:");
 
             //решайте задачу здесь
+            int[] newList = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 10};
+            Array.Sort(newList);
+            int uniqueCount = 1;
 
+            for (int i = 1; i < newList.Length; i++)
+            {
+                if (newList[i] != newList[i - 1])
+                {
+                    uniqueCount++;
+                }
+            }   
+
+            foreach (int number in newList)
+            {
+                Console.Write(number + " ");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Число уникальных элементов: " + uniqueCount);
             //не выходя за пределы
         }
     }
@@ -36,7 +67,31 @@ namespace LessonProject
             Console.WriteLine("РЕШЕНИЕ:");
 
             //решайте задачу здесь
-
+            int[] newList = {1, 2, 3, 3, 3, 3, 7, 7, 7, 7 };
+            int[] rateList = new int[newList.Length];
+            
+            for (int i = 0; i < newList.Length; i++)
+            {
+                int num = newList[i];
+                rateList[num]++;
+            }
+            
+            int maxRepeat  = 0;
+            for (int i = 0; i < rateList.Length; i++)
+            {
+                if (rateList[i] > maxRepeat)
+                {
+                    maxRepeat = rateList[i];
+                }
+            }
+            
+            for (int i = 0; i < rateList.Length; i++)
+            {
+                if (rateList[i] == maxRepeat)
+                {
+                    Console.WriteLine($"Число '{i}' появляется {maxRepeat} раза");
+                }
+            }
             //не выходя за пределы
         }
     }
@@ -50,6 +105,31 @@ namespace LessonProject
 
             //решайте задачу здесь
 
+            int[] oldList = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 10};
+            int K = 3;
+
+            Console.WriteLine("Исходный массив:");
+            foreach (var i in oldList)
+            {
+                Console.Write(i + " ");
+            }
+
+            int[] newList = new int[oldList.Length];
+            for (int i = 0; i < oldList.Length; i++)
+            {
+                newList[(i + K) % oldList.Length] = oldList[i];
+            }
+
+            for (int i = 0; i < oldList.Length; i++)
+            {
+                oldList[i] = newList[i];
+            }
+
+            Console.WriteLine("\nНовый массив:");
+            foreach (var i in oldList)
+            {
+                Console.Write(i + " ");
+            }
             //не выходя за пределы
         
         }
@@ -63,7 +143,27 @@ namespace LessonProject
             Console.WriteLine("РЕШЕНИЕ:");
 
             //решайте задачу здесь
-
+            int[] oneList = {1, 2, 3, 4, 5, 6, 7};
+            int[] twoList = {3, 4, 5, 6, 7, 8, 9};
+            Console.WriteLine("Первый массив:");
+            foreach (int num in oneList)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Второй массив:");
+            foreach (int num in twoList)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine();
+            var commonNumbers = oneList.Intersect(twoList).Distinct().ToArray();
+            Console.WriteLine("Числа, встречающиеся в обоих массивах:");
+            foreach (int num in commonNumbers)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine();
             //не выходя за пределы
         }
     }
