133c49c85b2e83ec7ce52ad5a98de66cef9b8f8b
Fellkings <fellkings51@gmail.com>
2024-04-23 21:44:13 +0300

    feat: complete tasks Lesson8 and Homework

--- Course/Lesson8/PracticeAB/Program.cs
+++ Course/Lesson8/PracticeAB/Program.cs
@@ -2,66 +2,105 @@
 
 class Program
 {
-
-    
     // Творим тут
+    // 1
+    static int add(int a, int b)
+    {
+        return a + b;
+    }
 
+    // 2
+    static bool isEven(int number)
+    {
+        return number % 2 == 0;
+    }
 
-    //1
-    // public static int Task1(int a, int b){
-    //     return (a + b);
-    // }
+    // 3
+    static string reverseString(string str)
+    {
+        char[] charArray = str.ToCharArray();
+        Array.Reverse(charArray);
+        return string.Join("", charArray);
+    }
 
-    // //2
-    // static Task2()
-    // {
+    // 4
+    static int findMax(int[] arr){
+        return arr.Max();
+    }
 
-    // }
+    // 5
+    static double calculateYearlySalary(double salary)
+    {
+        return salary * 12;
+    }
 
-    // //3
-    // static Task3()
-    // {
+    // 6
+    static double celsiusToFahrenheit(double celsius)
+    {
+        return celsius * 9/5 + 32;
+    }
 
-    // }
+    // 7
+    static int countVowels(string str)
+    {
+        string vowels = "УуЕеЭэОоАаЫыЯяИиЮю";
+        char[] vowels_ar = vowels.ToArray();
+        char[] chars = str.ToArray();
+        int count = 0;
 
-    // //4
-    // static Task4()
-    // {
+        foreach (char c in chars)
+        {
+            foreach (char gl in vowels)
+            {
+                if (c == gl)
+                {
+                    count++;
+                    break;
+                }
+            }
+        }
+        return count;
+    }   
 
-    // }
-
-    // // 5
-    // static Task5()
-    // {
-
-    // }
-
-    // // 6
-    // static Task6()
-    // {
-
-    // }
-
-    // //7
-    // static Task7()
-    // {
-
-    // }
-
-    // //8
-    // static Task8()
-    // {
-
-    // }
+    // 8
+    static int generatePassword(string passtohack)
+    {
+        int count = 0;
+        
+        for (int x = 0; x < 10; x++)
+        {
+            for (int y = 0; y < 10; y++)
+            {
+                for (int z = 0; z < 10; z++)
+                {
+                    for (int h = 0; h < 10; h++)
+                    {
+                        count++;
+                        string generatedpass = Convert.ToString(x) + Convert.ToString(y) + Convert.ToString(z) + Convert.ToString(h);
+                        if (generatedpass == passtohack)
+                        {
+                            Console.WriteLine("Ура! Вы взломали пароль, теперь вы хакер");
+                            return count-1;
+                        }
+                    }
+                }
+            }
+        }
+        
+        return count;
+    }
 
     static void Main(string[] args)
     {
-        // это функция мейн которая вызывает все отсальные функции для практики А и Б
+        // это функция мейн которая вызывает все остальные функции для практики А и Б
         //вызов первой функци.... и т.п.
-        Console.WriteLine(Task1(1, 2));
-
-    }
-    public static int Task1(int a, int b){
-        return (a + b);
+        Console.WriteLine(add(1, 3));
+        Console.WriteLine(isEven(7));
+        Console.WriteLine(reverseString("fell"));
+        Console.WriteLine(findMax(array_nums));
+        Console.WriteLine(calculateYearlySalary(2000));
+        Console.WriteLine(celsiusToFahrenheit(8));
+        Console.WriteLine(countVowels("Привет!"));
+        Console.WriteLine(generatePassword("8716"));
     }
 }
